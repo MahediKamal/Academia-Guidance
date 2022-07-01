1c3038ac0081d3db9aa43968665489357f936829
Mahedi Kamal <mahedikamal44@gmail.com>
2022-07-01 09:51:10 +0600

    project initialized
    Signed-off-by: Mahedi Kamal <mahedikamal44@gmail.com>

---
+++ Academia-Guidance/src/main/java/False_Authentication/Academia/Guidance/AcademiaGuidanceApplication.java
@@ -0,0 +1,13 @@
+package False_Authentication.Academia.Guidance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaGuidanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaGuidanceApplication.class, args);
+	}
+
+}
---
+++ Academia-Guidance/src/test/java/False_Authentication/Academia/Guidance/AcademiaGuidanceApplicationTests.java
@@ -0,0 +1,13 @@
+package False_Authentication.Academia.Guidance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaGuidanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
