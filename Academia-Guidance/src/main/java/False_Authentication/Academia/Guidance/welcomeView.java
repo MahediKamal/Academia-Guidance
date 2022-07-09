06e59ef998596b6407343107a780b274525f92e6
Mahedi Kamal <mahedikamal44@gmail.com>
2022-07-09 19:26:44 +0600

    thymleaf tmp checked
    Signed-off-by: Mahedi Kamal <mahedikamal44@gmail.com>

--- Academia-Guidance/src/main/java/False_Authentication/Academia/Guidance/welcomeView.java
+++ Academia-Guidance/src/main/java/False_Authentication/Academia/Guidance/welcomeView.java
@@ -3,11 +3,13 @@ package False_Authentication.Academia.Guidance;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+//@ResponseBody
 public class welcomeView {
     @GetMapping("/welcome")
-    public String welcime(Model model){
+    public String welcome(Model model){
         model.addAttribute("message", "hello world");
         return  "welcome";
     }
